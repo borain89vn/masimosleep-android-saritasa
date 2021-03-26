24c194b4c873ffe57396cb781ff7a577f924d971
Maxim Chavlytko <maxim.chavlytko@saritasa.com>
2021-03-26 19:36:36 +0700

    Clean code for shared preferences
    #SLPAPP-14

--- app/src/main/java/com/mymasimo/masimosleep/data/preferences/MasimoSleepPreferences.kt
+++ app/src/main/java/com/mymasimo/masimosleep/data/preferences/MasimoSleepPreferences.kt
@@ -5,7 +5,6 @@ import com.masimo.android.common.utility.PreferenceStore
 import com.mymasimo.masimosleep.MasimoSleepApp
 import java.util.*
 import kotlin.collections.ArrayList
-import kotlin.collections.HashSet
 
 private const val PREF_SELECTED_MODULE_ID = "pref.selected.module.id"
 private const val PREF_USER_NAME = "pref.user.name"
@@ -23,93 +22,42 @@ object MasimoSleepPreferences {
     var selectedModuleId: Long
         get() = store.getLong(PREF_SELECTED_MODULE_ID, 0L)
         set(value) {
-            if (value == 0L)
-                store.edit().remove(PREF_SELECTED_MODULE_ID).apply()
-            else store.edit().putLong(PREF_SELECTED_MODULE_ID, value).apply()
+            if (value == 0L) store.edit { remove(PREF_SELECTED_MODULE_ID) }
+            else store.edit { putLong(PREF_SELECTED_MODULE_ID, value) }
         }
 
     var name: String?
         get() = store.getString(PREF_USER_NAME, null)
-        set(value) {
-            store.edit(commit = true) {
-                putString(PREF_USER_NAME, value)
-            }
-        }
+        set(value) = store.edit { putString(PREF_USER_NAME, value) }
 
     var gender: String?
         get() = store.getString(PREF_USER_GENDER, null)
-        set(value) {
-            store.edit(commit = true) {
-                putString(PREF_USER_GENDER, value)
-            }
-        }
+        set(value) = store.edit { putString(PREF_USER_GENDER, value) }
 
     var birthdate: Long
         get() = store.getLong(PREF_USER_BIRTHDATE, Calendar.getInstance().timeInMillis)
-        set(value) {
-
-            store.edit(commit = true) {
-                putLong(PREF_USER_BIRTHDATE, value)
-            }
-        }
+        set(value) = store.edit { putLong(PREF_USER_BIRTHDATE, value) }
 
     var conditionList: ArrayList<String>?
         get() {
-
-            val set = store.getStringSet(PREF_USER_CONDITIONS,null)
-
-            return set?.let {
-                val list : ArrayList<String> = ArrayList()
-                for (string in set) {
-                    list.add(string)
-                }
-
-                list
-            }
+            val conditions = store.getStringSet(PREF_USER_CONDITIONS, null)
+            return conditions?.let { ArrayList(it) }
         }
-
-        set(value) {
-
-            val result = value?.let {
-                val set = HashSet<String>()
-                set.addAll(value)
-                set
-            }
-
-            store.edit(commit = true) {
-                putStringSet(PREF_USER_CONDITIONS, result)
-            }
-        }
+        set(value) = store.edit { putStringSet(PREF_USER_CONDITIONS, value?.toSet()) }
 
     var timeHour: Int
         get() = store.getInt(PREF_USER_TIME_HOUR, -1)
-        set(value) {
-            store.edit(commit = true) {
-                putInt(PREF_USER_TIME_HOUR, value)
-            }
-        }
+        set(value) = store.edit { putInt(PREF_USER_TIME_HOUR, value) }
 
     var timeMinute: Int
         get() = store.getInt(PREF_USER_TIME_MIN, -1)
-        set(value) {
-            store.edit(commit = true) {
-                putInt(PREF_USER_TIME_MIN, value)
-            }
-        }
+        set(value) = store.edit { putInt(PREF_USER_TIME_MIN, value) }
 
     var reminderTime: Int
         get() = store.getInt(PREF_USER_REMINDER_TIME, 0)
-        set(value) {
-            store.edit(commit = true) {
-                putInt(PREF_USER_REMINDER_TIME, value)
-            }
-        }
+        set(value) = store.edit { putInt(PREF_USER_REMINDER_TIME, value) }
 
     var eulaAccepted: Boolean
         get() = store.getBoolean(PREF_USER_EULA_ACCEPTED, false)
-        set(value) {
-            store.edit(commit = true) {
-                putBoolean(PREF_USER_EULA_ACCEPTED, value)
-            }
-        }
+        set(value) = store.edit { putBoolean(PREF_USER_EULA_ACCEPTED, value) }
 }
