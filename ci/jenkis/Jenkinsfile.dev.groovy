8712cd08bcf9ee50a2feed8f289e9d800a107b76
DanilaKazakevich <danila.kazakevich@saritasa.com>
2021-04-06 01:45:17 +0700

    Preparing for release jks

--- ci/jenkis/Jenkinsfile.dev.groovy
+++ ci/jenkis/Jenkinsfile.dev.groovy
@@ -1,3 +1,10 @@
+import groovy.json.JsonSlurper
+
+def config = [
+  vault_url: 'https://vault.saritasa.io/v1/project/data/masimosleep-android-develop',
+  firebaseId: "1:272253104842:android:a619de24f7e5106f33fa33",
+  firebaseGroup: "Masimo"
+]
 
 node("android"){
     try {
@@ -9,9 +16,25 @@ node("android"){
             }
         }
 
+
         stage('SCM'){
             checkout(scm)
         }
+        
+        // stage('Credentials') {
+        //     credentials = new JsonSlurper().parseText(
+        //     httpRequest(
+        //         customHeaders: [[maskValue: true, name: 'X-Vault-Token', value: "${env.VAULT_TOKEN}"]],
+        //         url: config.vault_url
+        //     ).content
+        //     ).data
+
+        //     fileContent = readFile('../../app/keystore/keystore.properties')
+        //     for (item in credentials) {
+        //     fileContent = fileContent.replace("%${item.key}%", "${item.value}")
+        //     }
+        //     writeFile(file: '../../app/keystore/keystore.properties', text: fileContent)
+        // }
 
         stage('Build:apk') {
             withEnv(["JAVA_HOME=${JAVA_HOME}"]) {
@@ -21,13 +44,15 @@ node("android"){
         
             }
         }
-
+        // stage('Deploy:firebase') {
+        //          sh("firebase appdistribution:distribute \
+        //        app/build/outputs/apk/emulator/release/MasimoSleep*.apk \
+        //         --app ${config.firebaseID} \
+        //         --groups ${config.firebaseGroup}")
+        // }
+        
         stage('Deploy:artifacts') {
-<<<<<<< HEAD
-            archiveArtifacts("app/build/outputs/apk/emulation/MasimoSleep*.apk")
-=======
             archiveArtifacts("app/build/outputs/apk/emulator/release/MasimoSleep*.apk")
->>>>>>> dev
         }
         
   } catch(error) {
