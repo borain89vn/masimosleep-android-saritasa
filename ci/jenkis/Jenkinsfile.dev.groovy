d2bcfd90a184dded973a2dc6270cbb014f5e1a60
DanilaKazakevich <danila.kazakevich@saritasa.com>
2021-04-05 21:24:52 +0700

    CI pilot

---
+++ ci/jenkis/Jenkinsfile.dev.groovy
@@ -0,0 +1,34 @@
+
+node("android"){
+    try {
+        JAVA_HOME = tool(name: 'jdk-latest', type: 'jdk')
+
+        stage('Clean'){
+            if ("${BUILD_CLEAN}" == "false") {
+                cleanWs()
+            }
+        }
+
+        stage('SCM'){
+            checkout(scm)
+        }
+
+        stage('Build:apk') {
+            withEnv(["JAVA_HOME=${JAVA_HOME}"]) {
+                ansiColor('xterm') {
+                    sh('./gradlew assembleEmulator')
+               }
+        
+            }
+        }
+
+        stage('Deploy:artifacts') {
+            archiveArtifacts("app/build/outputs/apk/emulator/release/MasimoSleep*.apk")
+        }
+        
+  } catch(error) {
+    currentBuild.result = 'FAILURE'
+    println("ERROR: ${error}")
+  }
+
+}
