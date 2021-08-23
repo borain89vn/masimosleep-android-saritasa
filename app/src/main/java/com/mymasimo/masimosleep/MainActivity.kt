32b013b39c7b22167d6eefd463e1069eb88bd1be
ThucLe <thuc.le@saritasa.com>
2021-08-23 13:45:05 +0700

    Native "back" button of android doesn't work in all app + minor UI corrections #SLPAPP-45

--- app/src/main/java/com/mymasimo/masimosleep/MainActivity.kt
+++ app/src/main/java/com/mymasimo/masimosleep/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.mymasimo.masimosleep
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onBackPressed() {
-        //disable back
+        //disable
+        val hostFragment = supportFragmentManager.fragments.first()
+        if (hostFragment.findNavController().popBackStack().not()){
+            finish()
+        }
     }
 }
