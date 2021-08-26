863f36b18e1a810a058ad6cf0329c3bd8b91c5cf
ThucLe <thuc.le@saritasa.com>
2021-08-26 08:18:09 +0700

    Disable back click when Screen is in HomeFragment #SLPAPP-69

--- app/src/main/java/com/mymasimo/masimosleep/MainActivity.kt
+++ app/src/main/java/com/mymasimo/masimosleep/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.mymasimo.masimosleep
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.FragmentNavigator
 import androidx.navigation.fragment.findNavController
+import com.mymasimo.masimosleep.ui.home.HomeFragment
+import com.mymasimo.masimosleep.ui.navigation.NavigationFragment
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onBackPressed() {
         //disable
         val hostFragment = supportFragmentManager.fragments.first()
-        if (hostFragment.findNavController().popBackStack().not()){
-            finish()
+        val navController = hostFragment.findNavController()
+        val currentDestination = (navController.currentDestination as FragmentNavigator.Destination).className
+        if (currentDestination != HomeFragment::class.qualifiedName) {
+            navController.popBackStack().not()
         }
     }
 }
